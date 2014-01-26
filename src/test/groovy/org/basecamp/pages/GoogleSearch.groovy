7536ec0d3738046a3a70075a24a03b60f5cc4317
Jesper Wermuth <jwermuth@gmail.com>
2014-01-26 23:13:53 +0100

    Knowledge of button on search page

--- src/test/groovy/org/basecamp/pages/GoogleSearch.groovy
+++ src/test/groovy/org/basecamp/pages/GoogleSearch.groovy
@@ -5,4 +5,15 @@ import geb.Page;
 class GoogleSearch extends Page {
 	static url = "http://google.dk"
 	static at = { title == "Google" }
+	static content = {
+		input  { $("input", id:"gbqfq", name:"q") }
+		searchButton  { $("button.gbqfba") } 
+	}
+	
+	
+	def search(q) {
+		input << q
+		searchButton.click()
+	}
+	
 }
